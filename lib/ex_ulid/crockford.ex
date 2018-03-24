42260e9c9a53a42d4622cf34d0ddb306093dbb1d
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2018-03-24 15:11:45 +0700

    Implement decoder

--- lib/ex_ulid/crockford.ex
+++ lib/ex_ulid/crockford.ex
@@ -3,8 +3,28 @@ defmodule ExULID.Crockford do
   This module provides data encoding and decoding functions
   according to [Crockford's Base32](http://www.crockford.com/wrmg/base32.html).
   """
-  @encoding '0123456789ABCDEFGHJKMNPQRSTVWXYZ'
   @bits 5
+  @encoding '0123456789ABCDEFGHJKMNPQRSTVWXYZ'
+
+  defmodule UnknownCharacterError do
+    # defexception message: "One of the given character is not part of the encoder's alphabet"
+    defexception [:message]
+
+    def exception(char) do
+      msg = "a character could not be decoded, got: #{inspect char}"
+      %UnknownCharacterError{message: msg}
+    end
+  end
+
+  defp encoding, do: @encoding
+
+  defp decoding do
+    @encoding
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn({char, index}, acc) ->
+         Map.put(acc, char, index)
+       end)
+  end
 
   @spec encode32(binary | integer) :: binary
   def encode32(data) when is_integer(data) do
@@ -13,34 +33,61 @@ defmodule ExULID.Crockford do
     |> encode32()
   end
   # Pad or remove any leading zero
+  def encode32(""), do: ""
   def encode32(data) do
     data = pad_bitlength(data, @bits)
     encode32("", data)
   end
+
   # Ignore leading zeros unless it's the only character
-  def encode32("", <<0::@bits>>), do: "0"
-  def encode32("", <<0::@bits, remainder::bitstring>>), do: encode32("", remainder)
+  defp encode32("", <<0::@bits>>), do: "0"
+  defp encode32("", <<0::@bits, remainder::bits>>), do: encode32("", remainder)
   # Main meaty part. Take the expected bits, convert to the encoding character,
   # then append the character to the accumulator.
-  def encode32(acc, <<bits::@bits, remainder::bitstring>>) do
+  defp encode32(acc, <<bits::@bits, remainder::bits>>) do
     acc
-    |> Kernel.<>(<<Enum.at(@encoding, bits)>>)
+    |> Kernel.<>(<<Enum.at(encoding(), bits)>>)
     |> encode32(remainder)
   end
   # Last remainder will be an empty binary,
   # the accumulator is now final, return it as the result.
-  def encode32(acc, <<>>), do: acc
+  defp encode32(acc, <<>>), do: acc
 
   defp pad_bitlength(data, bitlength) when rem(bit_size(data), bitlength) == 0, do: data
   defp pad_bitlength(data, bitlength) when rem(bit_size(data), bitlength) > 0 do
     remainder = rem(bit_size(data), bitlength)
-    <<a::size(remainder), remaining::bitstring>> = data
+    <<a::size(remainder), remaining::bits>> = data
 
     if a > 0 do
       missing_bits = bitlength - remainder
-      <<(<<0::size(missing_bits)>>), (data::binary)>>
+      <<(<<0::size(missing_bits)>>), (data::bits)>>
     else
-      <<remaining::bitstring>>
+      <<remaining::bits>>
     end
+  end
+
+  def decode32("0"), do: <<0>>
+  def decode32(string) do
+    string = String.to_charlist(string)
+    decode32("", string)
+  rescue
+    e in UnknownCharacterError -> {:error, e.message}
+  end
+
+  defp decode32(acc, ''), do: pad_bitlength(acc, 8)
+  defp decode32(acc, charlist) do
+    [char | remainder] = charlist
+    decode32(acc, remainder, char)
+  end
+  defp decode32(acc, remainder, char) do
+    mapped =
+      case Map.fetch(decoding(), char) do
+        {:ok, mapped} -> mapped
+        :error        -> raise(UnknownCharacterError, <<char>>)
+      end
+
+    <<_::3, data::5>> = :binary.encode_unsigned(mapped)
+
+    decode32(<<(acc::bits), data::5>>, remainder)
   end
 end
--- test/ex_ulid/crockford_test.exs
+++ test/ex_ulid/crockford_test.exs
@@ -28,7 +28,7 @@ defmodule ExULID.CrockfordTest do
     end
 
     # https://github.com/gbarr/Encode-Base32-Crockford/blob/master/t/base32.t
-    test "returns the encoded value the same as `gbarr/Encode-Base32-Crockford` library" do
+    test "returns the same encoded values as `gbarr/Encode-Base32-Crockford` library" do
       assert "2" == encode32(2)
       assert "10" == encode32(32)
       assert "A0" == encode32(320)
@@ -38,13 +38,13 @@ defmodule ExULID.CrockfordTest do
     end
 
     # https://github.com/jbittel/base32-crockford/blob/master/test.py
-    test "returns the encoded value the same as `jbittel/base32-crockford` library" do
+    test "returns the same encoded values as `jbittel/base32-crockford` library" do
       assert "16J" == encode32(1234)
     end
 
     # https://github.com/dflydev/dflydev-base32-crockford/blob/master/
     # tests/Dflydev/Base32/Crockford/CrockfordTest.php
-    test "returns the encoded value the same as `dflydev/dflydev-base32-crockford` library" do
+    test "returns the same encoded value as `dflydev/dflydev-base32-crockford` library" do
       assert "0" == encode32(0)
       assert "1" == encode32(1)
       assert "2" == encode32(2)
@@ -56,29 +56,82 @@ defmodule ExULID.CrockfordTest do
     end
   end
 
-  # describe "decode32!/1" do
-  #   test "returns the encoded value" do
-  #     assert "" == decode32!("")
-  #     assert "o" == decode32!("DW======")
-  #     assert "om" == decode32!("DXPG====")
-  #     assert "omi" == decode32!("DXPPJ===")
-  #     assert "omis" == decode32!("DXPPJWR=")
-  #     assert "omise" == decode32!("DXPPJWV5")
-  #     assert "omiseg" == decode32!("DXPPJWV5CW======")
-  #     assert "omisego" == decode32!("DXPPJWV5CXQG====")
-  #     assert <<161, 178, 195, 212, 229, 246, 120, 145>> == decode32!("M6SC7N75YSW92===")
+  describe "decode32/1" do
+    test "returns the encoded value" do
+      assert "" == decode32("")
+      assert "o" == decode32("3F")
+      assert "om" == decode32("VVD")
+      assert "omi" == decode32("6YVB9")
+      assert "omis" == decode32("1QPTTBK")
+      assert "omise" == decode32("DXPPJWV5")
+      assert "omiseg" == decode32("3FDNMQ6SB7")
+      assert "omisego" == decode32("VVDD5SPASVF")
+    end
 
-  #     assert <<161, 178, 195, 212, 229, 246, 120, 145>> ==
-  #              decode32!("m6sc7n75ysw92===", case: :lower)
+    # https://github.com/gbarr/Encode-Base32-Crockford/blob/master/t/base32.t
+    test "returns the same decoded values as `gbarr/Encode-Base32-Crockford` library" do
+      assert 2 == decode32("2") |> to_integer()
+      assert 32 == decode32("10") |> to_integer()
+      assert 320 == decode32("A0") |> to_integer()
+      assert 354715840941 == decode32("AABBCCDD") |> to_integer()
+      assert 128 == decode32("40") |> to_integer()
+      assert 500 == decode32("FM") |> to_integer()
+    end
 
-  #     assert <<161, 178, 195, 212, 229, 246, 120, 145>> ==
-  #              decode32!("M6Sc7n75YsW92===", case: :mixed)
-  #   end
+    # https://github.com/jbittel/base32-crockford/blob/master/test.py
+    test "returns the same decoded values as `jbittel/base32-crockford` library" do
+      assert :binary.encode_unsigned(1234) == decode32("16J")
+    end
 
-  #   test "returns :error on non-alphabet digit" do
-  #     assert :error == decode32("66KF")
-  #     assert :error == decode32("66ff")
-  #     assert :error == decode32("66FF", case: :lower)
-  #   end
-  # end
+    # https://github.com/dflydev/dflydev-base32-crockford/blob/master/
+    # tests/Dflydev/Base32/Crockford/CrockfordTest.php
+    test "returns the same decoded value as `dflydev/dflydev-base32-crockford` library" do
+      assert 0 == decode32("0") |> to_integer()
+      assert 1 == decode32("1") |> to_integer()
+      assert 2 == decode32("2") |> to_integer()
+      assert 194 == decode32("62") |> to_integer()
+      assert 456789 == decode32("DY2N") |> to_integer()
+      assert 398373 == decode32("C515") |> to_integer()
+      assert 519571 == decode32("FVCK") |> to_integer()
+      assert 3838385658376483 == decode32("3D2ZQ6TVC93") |> to_integer()
+    end
+
+    # Ref: https://github.com/ulid/spec
+    test "returns :error on non-alphabet digit (I, L, O, and U)" do
+      assert {:error, ~s(a character could not be decoded, got: "I")} = decode32("I")
+      assert {:error, ~s(a character could not be decoded, got: "L")} = decode32("L")
+      assert {:error, ~s(a character could not be decoded, got: "O")} = decode32("O")
+      assert {:error, ~s(a character could not be decoded, got: "U")} = decode32("U")
+
+      assert {:error, ~s(a character could not be decoded, got: "I")} = decode32("111IIIAAA")
+    end
+  end
+
+  defp to_integer(<<a::8>>) do
+    a
+  end
+  defp to_integer(<<_::8, _::8>> = data) do
+    <<int::integer-unsigned-16>> = data
+    int
+  end
+  defp to_integer(<<_::8, _::8, _::8>> = data) do
+    <<int::integer-unsigned-24>> = data
+    int
+  end
+  defp to_integer(<<_::8, _::8, _::8, _::8>> = data) do
+    <<int::integer-unsigned-32>> = data
+    int
+  end
+  defp to_integer(<<_::8, _::8, _::8, _::8, _::8>> = data) do
+    <<int::integer-unsigned-40>> = data
+    int
+  end
+  defp to_integer(<<_::8, _::8, _::8, _::8, _::8, _::8>> = data) do
+    <<int::integer-unsigned-48>> = data
+    int
+  end
+  defp to_integer(<<_::8, _::8, _::8, _::8, _::8, _::8, _::8>> = data) do
+    <<int::integer-unsigned-56>> = data
+    int
+  end
 end
